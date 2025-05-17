1be6e57d3d446e6482e7220432b214ef1abe1fda
bluefits <44682470+BlueFits@users.noreply.github.com>
2025-05-17 00:27:41 -0400

    Update openai.service.ts

--- dnd-server/src/openai/openai.service.ts
+++ dnd-server/src/openai/openai.service.ts
@@ -15,7 +15,15 @@ export class OpenaiService {
   async chat(prompt: ResponseInput) {
     const response = await this.openai.responses.create({
       model: 'gpt-4.1',
-      input: prompt,
+      input: [
+        /* These will eventually be the safeguards */
+        // {
+        //   role: 'system',
+        //   content: `You are the Dungeon Master of a grounded dark fantasy DnD campaign. Respond as if narrating the world in real-time.
+        //   Any other requests that does not relate to Dnd will not be allowed.`,
+        // },
+        ...prompt,
+      ],
     });
     return response;
   }
